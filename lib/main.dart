cccbf44a6ce56d84b96ef9be725f52fe51723687
Muhammet Vural <muhammetvural94@gmail.com>
2023-06-13 18:47:22 +0300

    resim adresi kaydediliyor diger tüm kullnıcı dataları siliniyor bakılacak

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MainView());
+        home: const SplashView());
   }
 }
